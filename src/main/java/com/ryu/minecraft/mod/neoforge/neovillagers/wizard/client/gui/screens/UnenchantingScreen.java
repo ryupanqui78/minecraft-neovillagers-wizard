85ecd815f33f2f86d1dc673a90ab45c9567a2501
Richard Yupanqui <rdyh0906@gmail.com>
2024-07-18 20:00:57 -0300

    Adding tooltip to Unenchanting screen

--- src/main/java/com/ryu/minecraft/mod/neoforge/neovillagers/wizard/client/gui/screens/UnenchantingScreen.java
+++ src/main/java/com/ryu/minecraft/mod/neoforge/neovillagers/wizard/client/gui/screens/UnenchantingScreen.java
@@ -22,6 +22,12 @@ public class UnenchantingScreen extends AbstractContainerScreen<UnenchantingMenu
     }
     
     @Override
+    public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+        this.renderTooltip(pGuiGraphics, pMouseX, pMouseY);
+    }
+    
+    @Override
     protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
         pGuiGraphics.blit(UnenchantingScreen.TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth,
                 this.imageHeight);
