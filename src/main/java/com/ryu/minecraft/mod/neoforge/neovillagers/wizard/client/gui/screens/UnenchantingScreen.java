0783916408e7a85b55f66c3a12a8c79b00129a6d
Richard Yupanqui <rdyh0906@gmail.com>
2025-10-13 22:21:46 -0300

    Upgrade to Minecraft 1.21.7

--- src/main/java/com/ryu/minecraft/mod/neoforge/neovillagers/wizard/client/gui/screens/UnenchantingScreen.java
+++ src/main/java/com/ryu/minecraft/mod/neoforge/neovillagers/wizard/client/gui/screens/UnenchantingScreen.java
@@ -15,10 +15,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 
-@OnlyIn(Dist.CLIENT)
 public class UnenchantingScreen extends AbstractContainerScreen<UnenchantingMenu> {
     
     private static final int SIZE_BUTTON_RESULT = 17;
